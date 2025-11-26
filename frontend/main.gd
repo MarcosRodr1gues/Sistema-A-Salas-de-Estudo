24a7c249cc12a1335c0820c518e18e723897bb4f
MarcosRodr1gues <marcosvvrodrigues01@gmail.com>
2025-11-26 19:42:57 -0300

    feat: Sistema de Login funcional (Backend + Frontend)

--- frontend/main.gd
+++ frontend/main.gd
@@ -1,29 +1,41 @@
-extends Node2D
+extends Control
 
-# Pega as referências dos nós que criamos
+@onready var input_email = $InputEmail
+@onready var input_senha = $InputSenha
+@onready var label_aviso = $LabelAviso
 @onready var http = $HTTPRequest
-@onready var label = $Label
-@onready var button = $Button
 
 func _ready():
-	# Conecta o clique do botão à função
-	button.pressed.connect(_on_button_pressed)
-	# Conecta a resposta do servidor à função
+	$BtnEntrar.pressed.connect(_on_btn_entrar_pressed)
 	http.request_completed.connect(_on_request_completed)
 
-func _on_button_pressed():
-	label.text = "Chamando o Python..."
-	# Faz o pedido para o seu servidor local
-	var url = "http://127.0.0.1:5000/teste"
-	var erro = http.request(url)
+func _on_btn_entrar_pressed():
+	var email = input_email.text
+	var senha = input_senha.text
 	
-	if erro != OK:
-		print("Erro ao tentar fazer a requisição!")
+	if email == "" or senha == "":
+		label_aviso.text = "Preencha todos os campos!"
+		return
+
+	label_aviso.text = "Conectando..."
+	
+	# Monta o pacote JSON para enviar
+	var dados = {"email": email, "senha": senha}
+	var headers = ["Content-Type: application/json"]
+	var json_string = JSON.stringify(dados)
+	
+	# Envia POST para o Python
+	http.request("http://127.0.0.1:5000/login", headers, HTTPClient.METHOD_POST, json_string)
 
 func _on_request_completed(result, response_code, headers, body):
-	# O servidor respondeu!
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	print(json)
-	
-	# Coloca a mensagem na tela
-	label.text = json["mensagem"]
+	if response_code == 200:
+		var json = JSON.parse_string(body.get_string_from_utf8())
+		if json["sucesso"] == true:
+			label_aviso.text = "Sucesso! Bem-vindo " + json["usuario_nome"]
+			# AQUI NO FUTURO VAMOS MUDAR DE CENA
+		else:
+			label_aviso.text = json["mensagem"]
+	elif response_code == 401:
+		label_aviso.text = "Senha incorreta!"
+	else:
+		label_aviso.text = "Erro no servidor: " + str(response_code)
